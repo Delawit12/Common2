e4075a415d61c679f63829a05ae9098b8cbe4ae1
Delawit12 <delawitgetu@gmail.com>
2024-02-15 13:37:58 -0800

    socket

--- codeBase/server/app.js
+++ codeBase/server/app.js
@@ -2,27 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 dotenv.config();
-import multerConfig from "./config/multer.js"; 
+import multerConfig from "./config/multer.js";
 
 import fs from "fs/promises";
 import sharp from "sharp";
 import path from "path";
 import os from "os";
 
-
-
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getServerIPAddress = () => {
   const ifaces = os.networkInterfaces();
-  let ipAddress = 'localhost';
+  let ipAddress = "localhost";
 
   Object.keys(ifaces).forEach((iface) => {
     ifaces[iface].forEach((ifaceDetails) => {
-      if (ifaceDetails.family === 'IPv4' && ifaceDetails.internal === false) {
+      if (ifaceDetails.family === "IPv4" && ifaceDetails.internal === false) {
         ipAddress = ifaceDetails.address;
       }
     });
@@ -31,17 +29,16 @@ const getServerIPAddress = () => {
   return ipAddress;
 };
 
-const host =  process.env.SERVER_HOST;
+const host = process.env.SERVER_HOST;
 const port = process.env.SERVER_PORT;
 
 import install from "./config/install.js";
 install();
-
-const server = express();
+import server from "./socket/index.js";
 
 // Enable CORS middleware before setting up routes
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://192.168.43.110:5173'],
+  origin: ["http://localhost:5173", "http://192.168.43.110:5173"],
   credentials: true,
 };
 server.use(cors(corsOptions));
@@ -50,8 +47,7 @@ server.use(cors(corsOptions));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(multerConfig.any());
-server.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
+server.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Define your routes
 import appRoute from "./routes/index.js";
@@ -64,7 +60,6 @@ server.get("/", (req, res) => {
 server.get("/test", (req, res) => {
   res.send("test url");
 });
-
 
 server.listen(port, host, () => {
   console.log("Server is running at http://${host}:${port}");
