3a1c4b755e1e09b9857ed79ebd945e132435718e
Delawit12 <delawitgetu@gmail.com>
2024-02-16 11:33:55 -0800

    user

--- codeBase/server/routes/user.route.js
+++ codeBase/server/routes/user.route.js
@@ -3,11 +3,7 @@ import userController from "../controllers/user.controller.js";
 import { auth, isAdmin } from "../auth/auth.js";
 const userRoutes = express.Router();
 
-userRoutes.post(
-  "/api/user/registerUser",
-  [auth, isAdmin],
-  userController.registerUser
-);
+userRoutes.post("/api/user/registerUser", userController.registerUser);
 userRoutes.post("/api/user", [auth, isAdmin], userController.confirmOTP);
 userRoutes.post("/api/user", [auth, isAdmin], userController.forgetPassword);
 userRoutes.post("/api/user", [auth, isAdmin], userController.newPassword);
