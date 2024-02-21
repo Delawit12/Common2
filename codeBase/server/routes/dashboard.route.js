6b106fb99dc510f8445c927b7f79ae508029257a
SenaKebe <152234422+SenaKebe@users.noreply.github.com>
2024-02-21 18:05:03 +0100

    cors from alll

---
+++ codeBase/server/routes/dashboard.route.js
@@ -0,0 +1,12 @@
+// import express from "express";
+// import dashboardController from "../controllers/dashboard.controller.js";
+// import { auth, isAdmin } from "../auth/auth.js";
+// const userRoutes = express.Router();
+
+// userRoutes.post("/api/user/deactivate", dashboardController.deactivateUser);
+// userRoutes.post("/api/user/delete/:id", dashboardController.deleteUser);
+// userRoutes.post("/api/user/assignRole", dashboardController.assignRole);
+// userRoutes.get("/api/user/gender", dashboardController.newPassword);
+// // userRoutes.post("/api/user/change", [auth], userController.changePassword);
+
+// export default userRoutes;
